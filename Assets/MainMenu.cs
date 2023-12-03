71ee73465312ffe3181357f218c97f71b51fffa7
pedroramoscosta00 <pedroramoscosta00@gmail.com>
2023-12-03 22:17:53 +0000

    Cenas de pedro

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void MenuPrincipal()
+    {
+        SceneManager.LoadSceneAsync(0);
+    }
+
+    public void LigarAR()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+}
---
+++ Assets/script.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+
+    public void BotaoRotaNau() 
+    {
+        //Correr codigo quando carregar no botão
+
+
+    }
+
+
+}
