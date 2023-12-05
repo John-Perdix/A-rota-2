838ee4d04f82e208a451f35d7659726dcf824a54
pedroramoscosta00 <pedroramoscosta00@gmail.com>
2023-12-05 16:34:41 +0000

    Invenatário (Completo)

--- Assets/DisplayInventory.cs
+++ Assets/DisplayInventory.cs
@@ -9,7 +9,7 @@ public class DisplayInventory : MonoBehaviour
     public int X_START;
     public int Y_START;
     public int X_SPACE_BETWEEN_ITEMS;
-    public int NUMBER_OF_COLUMNS;
+    public int NUMBER_OF_COLUMN;
     public int Y_SPACE_BETWEEN_ITEMS;
     Dictionary<InventorySlot, GameObject> itemsDisplayed = new Dictionary<InventorySlot, GameObject>();
     // Start is called before the first frame update
@@ -54,6 +54,6 @@ public class DisplayInventory : MonoBehaviour
     }
     public Vector3 GetPosition(int i)
     {
-        return new Vector3(X_START + (X_SPACE_BETWEEN_ITEMS * (i % NUMBER_OF_COLUMNS)), Y_START + (-Y_SPACE_BETWEEN_ITEMS * (i / NUMBER_OF_COLUMNS)), 0f);
+        return new Vector3(X_START + (X_SPACE_BETWEEN_ITEMS * (i % NUMBER_OF_COLUMN)), Y_START + (-Y_SPACE_BETWEEN_ITEMS * (i / NUMBER_OF_COLUMN)), 0f);
     }
 }
