1cf9f605d4f92eae3b63f7a1fe43677d2d12f2be
John-Perdix <joao_perdiz@outlook.com>
2023-12-29 22:50:53 +0000

    .

--- Assets/loadConfig.cs
+++ Assets/loadConfig.cs
@@ -9,7 +9,7 @@ public class loadConfig : MonoBehaviour
     
     public GameObject[] objetosInventario;
 
-    void Start()
+    /* void Start()
     {
 
         for (int i = 0; i < objetosInventario.Length; i++)
@@ -17,7 +17,27 @@ public class loadConfig : MonoBehaviour
             GameObject obj = Instantiate(objetosInventario[i], objetosInventario[i].transform.position, objetosInventario[i].transform.rotation);
             obj.transform.position = new Vector3(Random.Range(-10, 10), -2, Random.Range(-10, 10));
         }
+    } */
+
+    void Start()
+{
+    float radius = 5f;
+
+    for (int i = 0; i < objetosInventario.Length; i++)
+    {
+        GameObject obj = Instantiate(objetosInventario[i], objetosInventario[i].transform.position, objetosInventario[i].transform.rotation);
+
+        // Generate a random position
+        Vector3 randomPosition;
+        do
+        {
+            randomPosition = new Vector3(Random.Range(-10, 10), -3, Random.Range(-10, 10));
+        } while (Vector3.Distance(randomPosition, Vector3.zero) < radius);
+
+        // Set the object's position
+        obj.transform.position = randomPosition;
     }
+}
 
     // Update is called once per frame
     void Update()
